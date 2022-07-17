8e58b9dc95382275604b4f1d12d938fe6bd6746e
cindy917 <90430653+cindylovecoding@users.noreply.github.com>
2022-07-17 17:33:25 +0800

    Create main.cpp

---
+++ 72. Edit Distance/main.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+        word1 = "#" + word1;
+        word2 = "#" + word2;
+        auto dp = vector<vector<int>> (m+1,vector<int>(n+1));
+        for(int i=0;i<=m;i++)
+            dp[i][0] = i;
+        for(int j=0;j<=n;j++)
+            dp[0][j] = j;
+        for(int i = 1;i <=m;i++)
+            for(int j = 1;j <=n;j++){
+                if(word1[i]==word2[j])
+                    dp[i][j] = dp[i-1][j-1];
+                else
+                    dp[i][j] = min(dp[i-1][j] + 1,min(dp[i][j-1]+1,dp[i-1][j-1]+1));
+           }
+        return dp[m][n];
+    }
+};
